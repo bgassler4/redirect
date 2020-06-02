15bd196d983b8e851a0927ea91fa52b522240d7a
Brendan Gassler <bgassler4@gmail.com>
2020-06-02 17:28:08 +1000

    making a few updates for demo

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ exports.handler = async (event, context) => {
   console.log("Received event:", JSON.stringify(event, null, 2));
 
   //TODO: Persist Data
-  const redirectUrl = `https://usefyi.com/blog/`;
+  console.log(`Request received from url: ${event.requestUrl}`);
 
-  return redirectUrl;
+  const response = {
+    statusCode: 200,
+    data: {
+      redirectUrl: "https://usefyi.com/blog/",
+    },
+  };
+  return response;
 };
